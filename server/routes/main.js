5b916d9d1fb619265613cda910a01beea2e8c17f
aondrej ryan <aondrejryan11@gmail.com>
2023-10-18 12:50:35 -0700

    set up server and stuff

---
+++ server/routes/main.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+// random routes and shit go here 
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const http = require('http')
+const { Server } = require('socket.io')
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+//const tweetRoutes = require("./routes/tweet");
+
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" });
+
+// Passport config
+
+//Connect To Database
+connectDB();
+
+
+app.use(cors({
+  origin: 'http://localhost:2014',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:2014',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }
+})
+//Body Parsing
+app.use(express.urlencoded({ extended: true,  limit: '25mb'}));
+app.use(express.json({limit: '25mb'}));
+
+//Logging
+app.use(logger("dev"));
+
+// Setup Sessions - stored in MongoDB
+app.use(
+    session({
+      secret: "keyboard cat",
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  );
+  app.use(cookieParser("keyboard cat"))
+  // Passport middleware
+  app.use(passport.initialize());
+  app.use(passport.session());
+  require("./config/passport")(passport);
+  //Use flash messages for errors, info, ect...
+  app.use(flash());
+
+  
+   app.use("/", mainRoutes);
+
+  server.listen(process.env.PORT, () => {
+    console.log("Server is running, you better catch it!");
+  });
