38d2a3f38389146da20bea93b5781491e52f31d3
aondrej ryan <aondrejryan11@gmail.com>
2023-11-05 12:48:04 -0800

    working slowly but surely

--- server/controller/Plaid.js
+++ server/controller/Plaid.js
@@ -1,4 +1,4 @@
-const plaid = require('plaid')
+const plaid = require('plaid');
 const Trans = require("../model/Transaction");
 const User = require("../model/User");
 const plaidClient = new plaid.Client({
@@ -6,3 +6,24 @@ const plaidClient = new plaid.Client({
   secret: process.env.SANDBOX,
   env: plaid.environments.sandbox, 
 });
+
+module.exports = {
+    createPlaidToken:  (req,res) => {
+         plaidClient.env.createPublicToken({
+            user: {
+            client_user_id: 'unique-user-id', // Generate a unique user ID for each user
+            },
+            client_name: 'Your App Name',
+            products: ['auth', 'transactions'], // Specify the desired products (e.g., 'auth', 'transactions')
+            country_codes: ['US'], // Specify the country codes
+            language: 'en', // Specify the language
+        }, (err, response) => {
+            if (err) {
+            console.error(err);
+            return res.status(500).send('Error generating link token');
+            }
+            const linkToken = response.link_token;
+            res.json({ link_token: linkToken });
+            })
+    }
+}
