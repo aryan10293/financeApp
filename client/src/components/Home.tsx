f03bc17e46d72af1b54aa0198de1dfea1d214b50
aondrej ryan <aondrejryan11@gmail.com>
2023-11-14 02:42:08 -0800

    i may have found a resource

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,39 +1,39 @@
 import React from 'react'
 import { Fragment } from 'react'
 import Nav from './Nav'
-import { Configuration, PlaidEnvironments } from 'plaid';
+import {usePlaidLink, PlaidLinkOptions, PlaidLinkOnSuccess} from 'react-plaid-link';
 function Home() {
   const [linkToken, setLinkToken] = React.useState<any>('');
   const [userData, setUserData] = React.useState<any[]>([])
-
-  const testing = async (e:any) => {
-    e.preventDefault()
-    try {
-      const response = await fetch(`http://localhost:2014/getplaidtoken/${localStorage.getItem('token')}`, {
-        method: 'GET',
-        headers: {'Content-Type': 'application/json', 'Authorization': `${localStorage.getItem('token')}`}
-      })
-      const info = await response.json()
-      console.log(info)
-    //   if (window.Plaid) {
-    //   const handler = window.Plaid.create({
-    //     token: info.link_token,
-    //     onSuccess: handleSuccess,
-    //     onExit: handleExit,
-    //     // Add other options as needed
-    //   });
-
-    //   // Open Plaid Link
-    //   handler.open();
-    // }
-
-    //   // Open Plaid Link
-    //   handler.open();
-    // }
-    // } catch (error) {
-    //   console.error(error)
-    // }
-  }
+  const handleSuccess = (publicToken: string, metadata: any) => {
+    // Handle the obtained public token
+    console.log('Public Token:', publicToken);
+  };
+   const handleExit = (error: any, metadata: any) => {
+    // Handle the exit event
+    if (error) {
+      console.error('Plaid Link exit with error:', error);
+    }
+  };
+    const testing = async (e:any) => {
+      e.preventDefault()
+      try {
+        const response = await fetch(`http://localhost:2014/getplaidtoken/${localStorage.getItem('token')}`, {
+          method: 'GET',
+          headers: {'Content-Type': 'application/json', 'Authorization': `${localStorage.getItem('token')}`}
+        })
+        const info = await response.json()
+        const config: PlaidLinkOptions = {
+            onSuccess: handleSuccess,
+            onExit: handleExit,
+            onEvent: (eventName, metadata) => {},
+            token: info.link_token,
+          };
+          const { open, exit, ready } = usePlaidLink(config);
+      } catch(err) {
+        console.log(err)
+      }
+    } 
   return (
     <>
     <Nav page='home'/>
