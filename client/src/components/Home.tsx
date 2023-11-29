5fd3210c7b25560fb6a650a85b7a6ca817c3a535
aondrej ryan <aondrejryan11@gmail.com>
2023-11-29 01:01:03 -0800

    fuck this this app on to the next

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,66 +1,48 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import React from 'react'
-import { Fragment } from 'react'
-import Nav from './Nav'
-import {usePlaidLink, PlaidLinkOptions, PlaidLinkOnSuccess} from 'react-plaid-link';
-function Home() {
-  const [linkToken, setLinkToken] = React.useState<any>('');
-  const [count, setCount] = React.useState<number>(0)
-  // Fetch link token from your server
-  const fetchLinkToken = async () => {
-    try {
-      const response = await fetch(`http://localhost:2014/getplaidtoken/${localStorage.getItem('token')}`,{
-            method: 'POST',
-            headers: {'Content-Type': 'application/json', 'Authorization': `${localStorage.getItem('token')}`},
-          });
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data.link_token)
-          if(count === 0){
-            setLinkToken(data.link_token);
-            setCount(3)
-          }
-      } else {
-        console.error('Error fetching link token');
-      }
-    } catch (error) {
-      console.error('Error fetching link token:', error);
-    }
+import React, { useEffect, useState } from 'react';
+import { usePlaidLink } from 'react-plaid-link';
+const Home = () => {
+  const [linkToken, setLinkToken] = useState(null);
+  const generateToken = async () => {
+    const response = await fetch(`http://localhost:2014/getplaidtoken/${localStorage.getItem('token')}`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json', 'Authorization': `${localStorage.getItem('token')}`},
+    });
+    const data = await response.json();
+    console.log(data.link_token)
+    setLinkToken(data.link_token);
   };
-
-  // Trigger Plaid Link when link token is available
-  const { open, ready } = usePlaidLink({
-    token: linkToken,
-    onSuccess: (public_token, metadata) => {
-      console.log('success', public_token, metadata);
-      // send public_token to server
-    },
-  });
-
-   React.useEffect( () => {
-        const lol = () => { fetchLinkToken();}
-        lol()
-    }, []);
-  return (
-    <>
-    <Nav page='home'/>
-
-
-    <h1 className="bg-money-green text-white">
-      This is a money green background.
-    </h1>
-    {linkToken ? <button onClick={() =>  open()} disabled={!ready}>Connect to bank</button> : (
-      null
-    )}
-    {/* {publicToken ? (<PlaidAuth publicToken={publicToken} />) : (
-      <button onClick={() => open()} disabled={!ready}>
-        Connect a bank account
-      </button>} */}
-    </>
-  )
+  useEffect(() => {
+    generateToken();
+  }, []);
+  return linkToken != null ? <Link linkToken={linkToken} /> : <></>;
+};
+// LINK COMPONENT
+// Use Plaid Link and pass link token and onSuccess function
+// in configuration to initialize Plaid Link
+interface LinkProps {
+  linkToken: string | null;
 }
-
-export default Home
-
-
-/// turn the linktoken stuff into a use effect and lets see what that does tomorrow 
+const Link: React.FC<LinkProps> = (props: LinkProps) => {
+  const onSuccess = React.useCallback((public_token:any, metadata:any) => {
+    // send public_token to server
+    const response = fetch(`http://localhost:2014/exchangetoken`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ public_token }),
+    });
+    // Handle response ...
+  }, []);
+  const config: Parameters<typeof usePlaidLink>[0] = {
+    token: props.linkToken!,
+    onSuccess,
+  };
+  const { open, ready } = usePlaidLink(config);
+  return (
+    <button onClick={() => open()} disabled={!ready}>
+      Link account
+    </button>
+  );
+};
+export default Home;
